b1cc234b3c8b4ce600595cfa2b4ca2e59e4d9b85
Varun Muppidi <varunkumar0521@gmail.com>
2025-01-27 08:23:41 -0600

    updated pr stale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'Root', // This violates the app prefix and kebab-case rule
+  selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
   standalone: false,
 })
-export class App { // This violates the Component suffix rule
-  constructor() {
-    console.log('test'); // This will trigger a no-console warning if you add it to rules
-  }
+export class AppComponent {
+  constructor() {}
 }
