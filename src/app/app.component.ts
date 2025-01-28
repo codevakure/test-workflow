a0a27ef0d27842eb2b828d2bf874496ba2a93c16
Varun Muppidi <varunkumar0521@gmail.com>
2025-01-28 05:25:13 -0600

    made changes to the application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, ViewChild, ElementRef, OnInit, OnDestroy } from '@angular/core';
-import { BehaviorSubject, interval } from 'rxjs';
-import { DomSanitizer } from '@angular/platform-browser';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,31 +6,6 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['app.component.scss'],
   standalone: false,
 })
-export class AppComponent implements OnInit, OnDestroy {
-  @ViewChild('contentDiv') contentDiv!: ElementRef;
-  
-  // Security Issue: Using dangerous HTML content
-  userContent = '<script>alert("hack")</script>';
-  
-  // Memory Leak: Unmanaged subscription
-  counter$ = new BehaviorSubject<number>(0);
-  items = Array.from({length: 1000}, (_, i) => `Item ${i}`);
-  
-  constructor(private sanitizer: DomSanitizer) {}
-  
-  ngOnInit() {
-    // Memory Leak: subscription without cleanup
-    interval(1000).subscribe(val => {
-      this.counter$.next(val);
-    });
-
-    // Security Issue: bypassing security
-    this.contentDiv.nativeElement.innerHTML = this.userContent;
-    this.sanitizer.bypassSecurityTrustHtml(this.userContent);
-  }
-
-  // Performance Issue: Missing trackBy function
-  refreshItems() {
-    this.items = [...this.items];
-  }
+export class AppComponent {
+  constructor() {}
 }
