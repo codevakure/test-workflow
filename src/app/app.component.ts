195a79e98460b683c46e46138d4e30c18f128cbf
Varun Muppidi <varunkumar0521@gmail.com>
2025-01-28 17:50:08 -0600

    message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { SomeService } from './some.service';
 
 @Component({
   selector: 'app-root',
@@ -8,47 +7,7 @@ import { SomeService } from './some.service';
   standalone: false,
 })
 export class AppComponent {
-  varun!: string;
-  hello!: string;
-
-  constructor(private someService: SomeService) {
-    this.someService.getData().subscribe(data => {
-      console.log('Data:', data);
-      this.processData(data);
-    });
-  }
-
-  processData(data: any) {
-    // Empty method body
-  }
-
-  calculateResult() {
-    // Perform some complex calculation
-    const result = 2 + 3 * 4 / 5 - 6 * 7 + 8 / 9;
-    console.log('Result:', result);
-  }
-
-  ngOnInit() {
-    this.someService.getMoreData().subscribe(data => {
-      this.varun = data.varun;
-      this.hello = data.hello;
-      document.querySelector('#result')!.innerHTML = `<h1>${this.varun}</h1>`;
-    });
-  }
-
-  saveData() {
-    const url = 'http://api.example.com/data';
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        // Process the response
-      })
-      .catch(error => {
-        console.error('Error:', error);
-        // Handle the error
-      });
-  }
-
-  // Empty method
-  emptyMethod() {}
+   varun!: string;
+   hello!: string;
+  constructor() {}
 }
